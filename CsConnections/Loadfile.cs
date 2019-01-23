5b4954b745ffe02dddbfec53468d4ce3b0304731
mateusmsouza <mateusmsouza@outlook.com>
2019-01-23 12:04:43 -0200

    Tratando exceção

--- CsConnections/Loadfile.cs
+++ CsConnections/Loadfile.cs
@@ -8,7 +8,7 @@ namespace Time.LoadFile {
     {
 	    public static List<String> Initial()
 	    {
-            string path = @"C:/Users/Jeffersonn/source/repos/NewRepo/CsConnections/Connections.txt";
+            string path = @"C:/Users/Jefferson/source/repos/NewRepo/CsConnections/Connections.txt";
             List<string> toReturn = new List<string>();
             
             if (File.Exists(path))
